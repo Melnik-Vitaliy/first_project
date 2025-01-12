04c2b91a3d5c317af190279c133f0dc1d8f13754
Мельник Виталий <Barcas222@yandex.ru>
2025-01-12 20:39:16 +0300

    compleat version

--- test_file.py
+++ test_file.py
@@ -2,10 +2,7 @@ class CipherMaster:
     # Не изменяйте и не перемещайте эту переменную
     alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 
-    def __init__(self):
-        result = []ПОПРОБУЙ СДЕЛАТЬ ИНИТ ЧТОБЫ МАГИЧЕСКИЙ СТРЕНЖ ВЫВЕЛ ВСЕ КАК НУЖНО
-
-    def cipher(self, original_text, shift):
+    def cipher(self, original_text, shift) -> str:
         result = []
         low_original_text = str.lower(original_text)
         words = low_original_text.split()
@@ -17,22 +14,33 @@ class CipherMaster:
                     chip_simbol = self.alphabet.find(cimbol_words)
                     new_chip_simbol = int((chip_simbol + shift) % 33)
                     chip = self.alphabet[new_chip_simbol]
-                    sup_result += chip 
+                    sup_result += chip
                 else:
                     sup_result += cimbol_words
             big_sup_result += sup_result + ' '
         result.append(big_sup_result)
-        return result
+        return ''.join(result)
 
     def decipher(self, cipher_text, shift):
         result = []
-        for letter in cipher_text:
-            ...  # здесь ваш код
+        low_cipher_text = str.lower(cipher_text)
+        words = low_cipher_text.split()
+        big_sup_result: str = ''
+        for letter in words:
+            sup_result: str = ''
+            for cimbol_words in letter:
+                if cimbol_words != ',' and cimbol_words != '—':
+                    chip_simbol = self.alphabet.find(cimbol_words)
+                    new_chip_simbol = int((chip_simbol - shift) % 33)
+                    chip = self.alphabet[new_chip_simbol]
+                    sup_result += chip
+                else:
+                    sup_result += cimbol_words
+            big_sup_result += sup_result + ' '
+        result.append(big_sup_result)
         return ''.join(result)
 
-    def __str__():
-        return ' '.join(self.result)
-    
+
 cipher_master = CipherMaster()
 print(cipher_master.cipher(
     original_text='Однажды ревьюер принял проект с первого раза, с тех пор я его боюсь',
